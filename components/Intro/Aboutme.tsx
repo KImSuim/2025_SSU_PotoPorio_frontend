5059b08dce2f41050f4780d7ee28d206936230f9
KImSuim <suim7171@naver.com>
2025-09-23 20:49:02 +0900

    adout me 반응형

--- components/Intro/Aboutme.tsx
+++ components/Intro/Aboutme.tsx
@@ -3,11 +3,12 @@
 export default function AboutMe() {
   return (
     <>
-      <div className="text-[#FCF8F2] text-2xl z-20 relative px-[200px] pt-[80px] pb-[350px] bg-[#2E5D3A] flex flex-col gap-[20px]">
+      {/* <div className="top-0 bottom-0 text-[#FCF8F2] py-[25px] px-5 md:px-[55px] lg:px-[200px] sm:px-5 bg-[#0D1B11] flex justify-between"> */}
+      <div className="text-[#FCF8F2] text-2xl z-20 relative px-10 pt-[80px] sm:px-10 md:px-[100px] lg:px-[200px] pb-[150px] lg:pb-[350px] bg-[#2E5D3A] flex flex-col gap-[20px]">
         <div className="max-w-9xl text-left mx-auto ">
-          <div className="text-[80px]">About me</div>
-          <div className="font-aboutme font-pretendard text-[55px] mt-[10px]">"디자인과 사용자 경험을 연결하는 개발자"</div>
-          <div className="font-subtitle font-pretendard text-[35px] flex flex-col gap-6 mt-[30px]">
+          <div className="text-[65px] sm:text-[65px] md:text-[65px] lg:text-[80px]">About me</div>
+          <div className="font-aboutme font-pretendard text-[38px] sm:text-[38px] md:text-[45px] lg:text-[55px] mt-[10px]">"디자인과 사용자 경험을 연결하는 개발자"</div>
+          <div className="font-subtitle font-pretendard text-[28px] sm:text-[28px] md:text-[35px] lg:text-[35px] flex flex-col gap-6 mt-[20px]">
             <div>
               안녕하세요 웹 프론트엔드 개발자를 꿈꾸는 김수임입니다. <br />
               컴퓨터공학 전공과 SEO·마케팅 경험을 바탕으로, 웹을 다양한 시각에서 바라보는 개발자입니다. 현재는 프론트엔드 분야에 집중하며 실력을 쌓아가고 있습니다.
