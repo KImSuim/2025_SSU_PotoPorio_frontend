2fb6a15da7973bf51031ac47024675281ef10d5b
KImSuim <suim7171@naver.com>
2025-09-23 20:37:39 +0900

    footer 반응형

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -7,8 +7,8 @@ export default function Footer() {
   };
   return (
     <>
-      <div className="top-0 bottom-0 text-[#FCF8F2] py-[25px] px-[85px]  bg-[#0D1B11] flex justify-between">
-        <div>2025 PotoPorio</div>
+      <div className="top-0 bottom-0 text-[#FCF8F2] py-[25px] px-5 md:px-[55px] lg:px-[85px] sm:px-5 bg-[#0D1B11] flex justify-between">
+        <div className="hidden md:block text-base">2025 PotoPorio</div>
         <div className="cursor-pointer hover:drop-shadow-[0_0_10px_white] " onClick={handleCopy} title="클릭하면 복사됩니다">
           suim5748@gmail.com
         </div>
